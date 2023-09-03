cd456c5891f3b8e37e107e0544ab3e0a431df745
Eyüp Emre Erçin <122103564+Eercin@users.noreply.github.com>
2023-09-04 01:21:52 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
 import Project from './Pages/Project/Project';
